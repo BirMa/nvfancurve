2300ba4c07e7d5ca5ba02431db73579239d1f35f
Birnthaler Martin <birnthalermartin@gmail.com>
2024-02-11 03:16:11 +0000

    improve "rounding" of fan speeds for the two fan values

--- src/main.rs
+++ src/main.rs
@@ -290,12 +290,20 @@ mod tests {
 fn set_nv_fans(fan: f32, fan_min: f32) -> Result<(), String> {
     call_xhost_add()?;
 
-    // "Smooth" the int van pct value out over the two available fan speeds
-    let fan0 = (fan.ceil() as i8).min(100); // TODO - these two coult "round" more cleverly
-    let fan1 = (fan.floor() as i8).max(fan_min as i8);
+    // "Smooth" the int van pct value out across the two available fan integer speeds
+    let low = 0.2;
+    let high = 0.7;
 
-    log::info!("setting fans to ({:}%, {:}%)", fan0, fan1);
-    if let Err(mut err) = call_nv_settings(fan0, fan1) {
+    let ceil = (fan.ceil() as i8).min(100);
+    let floor = (fan.floor() as i8).max(fan_min as i8);
+    let rem = fan % 1.0;
+    let fan_low = if rem > high { ceil } else { floor };
+    let fan_high = if rem > low { ceil } else { floor };
+
+    log::debug!("\"rounding\" {:?} to {:?} and {:?}", fan, fan_low, fan_high);
+
+    log::info!("setting fans to ({:}%, {:}%)", fan_low, fan_high);
+    if let Err(mut err) = call_nv_settings(fan_low, fan_high) {
         if let Err(err_inner) = call_xhost_remove() {
             err = format!(
                 "{} happened, but during cleanup {} happened as well!",
